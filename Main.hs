97a8069fd078663ec93d416c8ea2d91aa38753d8
Valentin Golev <vgolev@trifacta.com>
2018-08-06 22:00:40 +0200

    working

---
+++ Main.hs
@@ -0,0 +1,202 @@
+{-# LANGUAGE ExistentialQuantification, FlexibleInstances, TypeFamilies #-}
+
+import Codec.Picture
+import Data.Vec3
+import Data.Maybe
+import Data.List
+import Data.Array
+import Data.Function
+import Control.Monad.Random
+import Control.Monad.Primitive
+import Control.Monad.Identity
+import Debug.Trace
+
+
+import Normal
+
+
+-- type Color = (Double, Double, Double)
+type Color = CVec3
+
+res :: (Int, Int)
+res = (300, 200)
+kRes :: Double
+kRes = fromIntegral $ uncurry min $ res
+
+data Ray = Ray {origin :: CVec3, direction :: CVec3}
+
+data Camera = Camera {
+  cOrigin :: CVec3,
+  cHorizontal :: CVec3,
+  cVertical :: CVec3,
+  cLowerLeftCorner :: CVec3
+}
+
+camera = Camera (CVec3 0 0 0) (CVec3 3 0 0) (CVec3 0 (-3) 0) (CVec3 (-2) 2 (-1))
+
+-- y :: Vec3 a -> Double
+y v = y'
+  where (_, y', _) = toXYZ v
+
+(*.) :: Double -> CVec3 -> CVec3
+k *. v = fromXYZ (x * k, y * k, z * k)
+  where (x, y, z) = toXYZ v
+
+
+sky :: Ray -> Color
+sky r = ((1.0 - t) *. CVec3 1 1 1) <+> (t *. CVec3 0.5 0.7 1.0)
+  where un = normalize $ direction r
+        t = (y un + 1) * 0.5
+
+objects :: [Hitable_]
+objects = [
+  MkHitable $ Sphere (CVec3 0.7 0 (-1)) 0.5,
+  MkHitable $ Sphere (CVec3 0 (-100.5) (-1)) 100 ]
+
+maxFloat :: Double
+maxFloat = fromIntegral $ snd $ floatRange (0.5::Double)
+--
+-- color' :: RandomGen g => Ray -> Rand g Color
+-- color' r = case (hit objects r 0 maxFloat) of
+--   Just (Hit t p n) -> return $ 0.5 *. (CVec3 1 1 1 <+> n)
+--   -- Just (Hit t p n) ->
+--   --   do ru <- randomInUnitBall
+--   --      let target = p <+> n <+> ru
+--   --      cl <- color $ Ray p (target <-> p)
+--   --      return $ 0.5 *. cl
+--   Nothing -> return $ sky r 
+
+
+color :: RandomGen g => Ray -> Rand g Color
+color r = case (hit objects r 0.00001 maxFloat) of
+  -- Just (Hit t p n) -> 0.5 *. (CVec3 1 1 1 <+> n)
+  Just (Hit t p n) ->
+    do ru <- randomInUnitBall
+       let target = p <+> n <+> ru
+       cl <- color $ Ray p (target <-> p)
+       return $ 0.5 *. cl
+  Nothing -> return $ sky r
+
+data Sphere = Sphere CVec3 Double
+data Hit = Hit {
+  hitT :: Double,
+  hitP :: CVec3,
+  hitNormal :: CVec3
+}
+
+frac :: Double -> Double
+frac = snd . properFraction
+
+randomInUnitBall :: RandomGen g => Rand g CVec3
+randomInUnitBall =
+  -- do (a:b:c:_) <- getRandomRs (0::Double, 1)
+  --    let p = (2.0 *. CVec3 a b c) <-> CVec3 1 1 1
+  --    if norm p >= 1 then randomInUnitBall else return p
+  do --(x:y:z:_) <- normals
+     (x:y:z:_) <- getRandomRs (0::Double, 1)
+     r <- getRandomR (0::Double, 1)
+     let p = r * sqrt (x*x + y*y + z*z)
+     return $ (1 / p) *. CVec3 x y z
+
+
+rayPointAt :: Ray -> Double -> CVec3
+rayPointAt (Ray o d) t = o <+> (t *. d)
+
+renderUV :: RandomGen g => Double -> Double -> Rand g Color
+-- renderUV u v = CVec3 u v 0.2
+renderUV u v = color $ Ray (cOrigin camera) (cLowerLeftCorner camera <+> (u *. (cHorizontal camera)) <+> (v *. (cVertical camera)))
+
+colorToPixel :: Color -> PixelRGB8
+colorToPixel c =
+  let (r, g, b) = toXYZ c
+  in PixelRGB8 (fromInteger $ floor $ r * 255.9) (fromInteger $ floor $ g * 255.9) (fromInteger $ floor $ b * 255.9)
+
+renderOnce :: RandomGen g => Int -> Int -> Rand g Color
+renderOnce x y =
+  do (a:b:_) <- getRandomRs (0::Double, 1)
+     renderUV ((a + fromIntegral x) / kRes) ((b + fromIntegral y) / kRes)
+
+render :: RandomGen g => Int -> Int -> Rand g PixelRGB8
+render x y = colorToPixel <$> ((1 / passes) *.) <$> rpass passes
+  where
+    passes = 20
+    rpass 0 = renderOnce x y
+    rpass i = renderOnce x y >>= \v1 -> (rpass (i - 1) >>= \v2 -> return (v1 <+> v2))
+
+vsum xs = foldr1 (<+>) xs
+--
+-- renderAntialiased :: Int -> Int -> PixelRGB8
+-- renderAntialiased x y = colorToPixel $ (1 / (fromIntegral $ length points)) *. vsum points
+--   where
+--     points =
+--       do i <- [0.1..0.9]
+--          j <- [0.1..0.9]
+--          return $ renderUV ((i + fromIntegral x) / kRes) ((j + fromIntegral y) / kRes)
+
+type ImgBuf = Array (Int, Int) PixelRGB8
+
+-- mmm :: RandT g [] a -> RandT g Identity [a]
+-- mmm r =
+
+
+genImageBuf :: Int -> Int -> IO ImgBuf
+genImageBuf w h = array ((0, 0), (w, h)) <$> lsIO
+  where
+    -- ls :: RandT g Identity [((Int, Int), PixelRGB8)]
+    ls = sequence (
+          do i <- [0..w]
+             j <- [0..h]
+             let v = render i j
+             return ((\c -> ((i, j), c)) <$> v))
+
+    lsIO :: IO [((Int, Int), PixelRGB8)]
+    lsIO = runIdentity <$> evalRandT ls <$> getStdGen
+
+genImageF :: Int -> Int -> IO (Int -> Int -> PixelRGB8)
+genImageF w h = f <$> genImageBuf w h
+  where
+    -- f b x y | trace ((show x) ++ " " ++ (show y)) False = undefined
+    f b x y = b ! (x, y)
+
+
+main :: IO ()
+main = do imF <- genImageF (fst res) (snd res)
+          let im = (generateImage imF (fst res) (snd res))
+          -- im <- evalRandIO rim
+          -- print (renderUV 0 0)
+          -- print (render 0 0)
+          -- print (colorToPixel $ CVec3 0.8 0.7 1.0)
+          writePng "./image.png" im
+
+
+class Hitable a where
+  hit :: a -> Ray -> Double -> Double -> Maybe Hit
+
+data Hitable_ = forall a . Hitable a => MkHitable a
+
+instance Hitable Sphere where
+  hit (Sphere sc sr) r@(Ray org dir) mn mx =
+    let oc = org <-> sc
+        a = dir .* dir
+        b = 2 * oc .* dir
+        c = (oc .* oc) - (sr * sr)
+        dsc = b * b - 4 * a * c
+        hf x = Just $ Hit x p n
+          where
+            p = rayPointAt r x
+            -- n = normalize $ p <-> CVec3 0 0 (-1)
+            n = (1 / sr) *. (p <-> sc)
+    in if dsc < 0 then Nothing
+       else let x0 = ( - b - sqrt dsc ) / (2 * a)
+                x1 = ( - b + sqrt dsc ) / (2 * a)
+            in if x0 >= mn && x0 <= mx then hf x0
+               else if x1 >= mn && x1 <= mx then hf x1
+                 else Nothing
+
+
+
+instance Hitable Hitable_ where
+ hit (MkHitable a) r mn mx = hit a r mn mx
+
+instance Hitable [Hitable_] where
+  hit objects r mn mx = listToMaybe $ sortBy (compare `on` hitT) $ catMaybes $ map (\o -> hit o r mn mx) objects
---
+++ Normal.hs
@@ -0,0 +1,26 @@
+module Normal (normals) where
+
+import Control.Monad.Random
+
+
+-- Normal distribution approximation
+-- ---------------------------------
+-- | Box-Muller method for generating two normally distributed
+-- independent random values from two uniformly distributed
+-- independent random values.
+boxMuller :: Floating a => a -> a -> (a,a)
+boxMuller u1 u2 = (r * cos t, r * sin t) where r = sqrt (-2 * log u1)
+                                               t = 2 * pi * u2
+
+
+-- | Convert a list of uniformly distributed random values into a
+-- list of normally distributed random values. The Box-Muller
+-- algorithms converts values two at a time, so if the input list
+-- has an uneven number of element the last one will be discarded.
+boxMullers :: Floating a => [a] -> [a]
+boxMullers (u1:u2:us) = n1:n2:boxMullers us where (n1,n2) = boxMuller u1 u2
+boxMullers _          = []
+
+
+normals :: (MonadRandom m, Random a, Floating a) => m [a]
+normals = boxMullers <$> getRandoms
