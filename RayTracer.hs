7765b7da4d100b813050d05bfcdb1c2d4cd7a416
Valentin Golev <vgolev@trifacta.com>
2018-08-08 19:27:02 +0200

    colorK

--- RayTracer.hs
+++ RayTracer.hs
@@ -45,17 +45,8 @@ sky r = ((1.0 - t) *. CVec3 1 1 1) <+> (t *. CVec3 0.5 0.7 1.0)
   where un = normalize $ direction r
         t = (y un + 1) * 0.5
 
---
--- color' :: RandomGen g => Ray -> Rand g Color
--- color' r = case (hit objects r 0 maxFloat) of
---   Just (Hit t p n) -> return $ 0.5 *. (CVec3 1 1 1 <+> n)
---   -- Just (Hit t p n) ->
---   --   do ru <- randomInUnitBall
---   --      let target = p <+> n <+> ru
---   --      cl <- color $ Ray p (target <-> p)
---   --      return $ 0.5 *. cl
---   Nothing -> return $ sky r
---
+
+
 color :: RandomGen g => Ray -> Rand g Color
 color r = case (hit objects r 0.00001 maxFloat) of
   -- Just (Hit t p n) -> 0.5 *. (CVec3 1 1 1 <+> n)
@@ -65,6 +56,16 @@ color r = case (hit objects r 0.00001 maxFloat) of
        cl <- color $ Ray p (target <-> p)
        return $ mapv (0.5 *) cl
   Nothing -> return $ sky r
+
+colorK :: RandomGen g => Double -> Ray -> Rand g Color
+colorK k r = case (hit objects r 0.00001 maxFloat) of
+  -- Just (Hit t p n) -> 0.5 *. (CVec3 1 1 1 <+> n)
+  Just (Hit t p n) ->
+    do ru <- randomInUnitBall
+       let target = p <+> n <+> ru
+       cl <- colorK (k/2) $ Ray p (target <-> p)
+       return cl
+  Nothing -> return $ mapv (k*) $ sky r
 
 
 
@@ -81,5 +82,5 @@ traceColorK objects pw ray = case (hit objects ray 0.000001 maxFloat) of
 
 traceColor :: RandomGen g => [Hitable_] -> Ray -> Rand g Color
 -- traceColor objects r = traceColorK objects 1 r
-traceColor objects r = color r
+traceColor objects r = colorK 1 r
 -- traceColor _ r = return $ sky r
