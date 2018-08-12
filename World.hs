025ab81727a75e665fd892f1adfb992d71249c77
Valentin Golev <vgolev@trifacta.com>
2018-08-12 22:18:33 +0200

    simplify

--- World.hs
+++ World.hs
@@ -10,13 +10,16 @@ import Hitable
 
 import RTMonad
 
+sphere m c r = MkHitable $ Sphere m c r
+
+
 objects :: [Hitable_]
 objects = [
-  MkHitable $ Sphere (mkLambertian $ CVec3 0.1 0.2 0.5) (CVec3 0 0 (-1)) 0.5,
-  MkHitable $ Sphere (mkLambertian $ CVec3 0.8 0.8 0.0) (CVec3 0 (-100.5) (-1)) 100,
-  MkHitable $ Sphere (mkMetal 0.3 $ CVec3 0.8 0.6 0.2) (CVec3 1 0 (-1)) 0.5,
-  -- MkHitable $ Sphere (mkMetal 1.0 $ CVec3 0.8 0.8 0.8) (CVec3 (-1) 0 (-1)) 0.5
-  MkHitable $ Sphere (mkDielectric 1.5) (CVec3 (-1) 0 (-1)) (-0.45)
+  sphere (mkLambertian $ CVec3 0.1 0.2 0.5) (CVec3 0 0 (-1)) 0.5,
+  sphere (mkLambertian $ CVec3 0.8 0.8 0.0) (CVec3 0 (-100.5) (-1)) 100,
+  sphere (mkMetal 0.3 $ CVec3 0.8 0.6 0.2) (CVec3 1 0 (-1)) 0.5,
+  -- sphere (mkMetal 1.0 $ CVec3 0.8 0.8 0.8) (CVec3 (-1) 0 (-1)) 0.5
+  sphere (mkDielectric 1.5) (CVec3 (-1) 0 (-1)) (-0.45)
   ]
 
 randomWorld :: RT ()
@@ -24,11 +27,11 @@ randomWorld :: RT ()
 randomWorld = ((typical++) <$> concat <$> sequence randList) >>= \w -> setSettings (Settings w)
   where
     typical = [
-      MkHitable $ Sphere (mkLambertian $ CVec3 0.5 0.5 0.5) (CVec3 0 (-1000) 0) 1000,
-      -- MkHitable $ Sphere (mkDielectric 1.5) (CVec3 0 0 0) 1,
-      MkHitable $ Sphere (mkDielectric 1.5) (CVec3 0 1 0) 1,
-      MkHitable $ Sphere (mkLambertian $ CVec3 0.4 0.2 0.1) (CVec3 (-4) 1 0) 1,
-      MkHitable $ Sphere (mkMetal 0 $ CVec3 0.7 0.6 0.5) (CVec3 4 1 0) 1]
+      sphere (mkLambertian $ CVec3 0.5 0.5 0.5) (CVec3 0 (-1000) 0) 1000,
+      -- sphere (mkDielectric 1.5) (CVec3 0 0 0) 1,
+      sphere (mkDielectric 1.5) (CVec3 0 1 0) 1,
+      sphere (mkLambertian $ CVec3 0.4 0.2 0.1) (CVec3 (-4) 1 0) 1,
+      sphere (mkMetal 0 $ CVec3 0.7 0.6 0.5) (CVec3 4 1 0) 1]
 
     randList :: [RT [Hitable_]]
     randList = (flip map) [(a, b) | a <- [-11..11], b <- [-11..11]] (uncurry genSphere)
@@ -42,10 +45,10 @@ randomWorld = ((typical++) <$> concat <$> sequence randList) >>= \w -> setSettin
         if (norm (center <-> CVec3 4 0.2 0) > 0.9)
           then return $ return $ case () of
                                   () | mt < 0.6 ->
-                                      MkHitable $ Sphere (mkLambertian $ CVec3 (r1 * r2) (r3 * r4) (r5 * r6)) center 0.2
+                                      sphere (mkLambertian $ CVec3 (r1 * r2) (r3 * r4) (r5 * r6)) center 0.2
                                   () | mt < 0.75 ->
-                                      MkHitable $ Sphere (mkMetal (0.5 * r4) $ CVec3 (0.5 * (1 + r1)) (0.5 * (1 + r2)) (0.5 * (1 + r3))) center 0.2
-                                  () | otherwise -> MkHitable $ Sphere (mkDielectric 1.5) center 0.2
+                                      sphere (mkMetal (0.5 * r4) $ CVec3 (0.5 * (1 + r1)) (0.5 * (1 + r2)) (0.5 * (1 + r3))) center 0.2
+                                  () | otherwise -> sphere (mkDielectric 1.5) center 0.2
           else return []
 
 data Sphere = Sphere Material CVec3 Double
