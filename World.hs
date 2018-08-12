19fabee5b729a7007ed1ea969662b38fc33fa545
Valentin Golev <vgolev@trifacta.com>
2018-08-12 19:40:18 +0200

    typical

--- World.hs
+++ World.hs
@@ -19,8 +19,13 @@ objects = [
   ]
 
 randomWorld :: RandomGen g => Rand g [Hitable_]
-randomWorld = concat <$> sequence randList
+randomWorld = (typical++) <$> concat <$> sequence randList
   where
+    typical = [
+      MkHitable $ Sphere (mkLambertian $ CVec3 0.5 0.5 0.5) (CVec3 0 (-1000) 0) 1000,
+      MkHitable $ Sphere (mkDielectric 1.5) (CVec3 0 1 0) 1,
+      MkHitable $ Sphere (mkLambertian $ CVec3 0.4 0.2 0.1) (CVec3 (-4) 1 0) 1,
+      MkHitable $ Sphere (mkMetal 0 $ CVec3 0.7 0.6 0.5) (CVec3 4 1 0) 1]
     -- randList :: [Rand g [Hitable_]]
     randList = (flip map) [(a, b) | a <- [-11..11], b <- [-11..11]] (uncurry genSphere)
 
