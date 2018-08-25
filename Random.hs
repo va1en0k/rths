213e76308da2e4227e7377781374b7f97588690f
Valentin Golev <vgolev@trifacta.com>
2018-08-25 04:28:33 +0200

    -1 idiot

--- Random.hs
+++ Random.hs
@@ -11,7 +11,7 @@ randomInUnitBall =
   --    let p = (2.0 *. CVec3 a b c) <-> CVec3 1 1 1
   --    if norm p >= 1 then randomInUnitBall else return p
   do --(x:y:z:_) <- normals
-     (x:y:z:_) <- getRandomRs (0::Double, 1)
-     r <- getRandomR (0::Double, 1)
+     (x:y:z:_) <- getRandomRs (-1::Double, 1)
+     r <- getRandomR (-1::Double, 1)
      let p = r * sqrt (x*x + y*y + z*z)
      return $ (1 / p) *. CVec3 x y z
