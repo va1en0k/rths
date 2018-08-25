3625752298618c26d4d8308c52c9e0e2b76230e8
Valentin Golev <vgolev@trifacta.com>
2018-08-25 18:19:20 +0200

    parrender

--- Parallel/Render.hs
+++ Parallel/Render.hs
@@ -73,6 +73,7 @@ genImageBuf w h = array ((0, 0), (w, h)) <$> lsRT
     lsRT = do
       (Settings w e s) <- getSettings
       hits <- runIO $ runGeometryShader e s (map asSphere w) allRays
+      runIO $ print hits
       let ps = map (colorToPixel . hitNormal . fromMaybe (Hit undefined undefined (CVec3 0 0 0) undefined)) hits
       return $ zip allPixels ps
     -- ls = mapWithProgressBar (uncurry $ renderPixelOnShader world) allPixels
