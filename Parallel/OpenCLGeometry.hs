ae8c1a76afdbc611e85292e27b0ca7bc09ecdfde
Valentin Golev <vgolev@trifacta.com>
2018-08-25 16:41:53 +0200

    opencl...

---
+++ Parallel/OpenCLGeometry.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Parallel.OpenCLGeometry where
+
+import Data.String.Interpolate
+-- import           Language.C.Quote.OpenCL         (cfun)
+
+import Data.Vec3
+
+import Types
+import World
+import Parallel.Shaders hiding (main)
+
+sphereSource :: String
+sphereSource = [i|
+    struct ray
+    {
+        float3 origin;
+        float3 direction;
+    };
+
+    struct hit
+    {
+        float t;
+        float3 point;
+        float3 normal;
+    };
+
+    bool sphereHit_1(struct ray r, float from, float to, struct hit * hit);
+
+    kernel void doubleArray(
+        global float8 *frayIn,
+        global float8 *fout
+    ) {
+
+        int i = get_global_id(0);
+
+        struct ray rayIn = {
+          {frayIn[i][0], frayIn[i][1], frayIn[i][2]},
+          {frayIn[i][3], frayIn[i][4], frayIn[i][5]}
+        };
+
+        struct hit h;
+        if (!sphereHit_1(rayIn, -10000, 100000, &h)) {
+          h.t = -22;
+        }
+
+        /*struct hit h = {
+          rayIn.direction.x + rayIn.direction.y/10,
+          {rayIn.direction.y + rayIn.direction.z/10,4,5},
+          {1,2,3}
+        };*/
+
+        fout[i] = (float8)(
+          h.t,
+          h.point[0], h.point[1], h.point[2],
+          h.normal[0], h.normal[1], h.normal[2],
+          -1
+        );
+    }
+|]
+
+v (CVec3 x y z) = [i|(float3)(#{x}, #{y}, #{z})|]
+
+compileSphere id (Sphere _ center radius) = [i|
+
+bool sphereHit_#{id}(struct ray r, float from, float to, struct hit * hit) {
+  float3 oc = r.origin - #{v center};
+  float a = dot(r.direction, r.direction);
+  float b = 2.0 * dot(oc, r.direction);
+  float c = dot(oc, oc) - #{radius * radius};
+  float dsc = b * b - 4.0 * a * c;
+
+  if (dsc < 0.0) {
+    hit->t = dsc;
+    hit->point = oc;
+    hit->normal = r.origin;
+    return false;
+  }
+  float sqDsc = sqrt(dsc);
+  float x = - b - sqDsc / 2.0 * a;
+  if (!(x >= from && x <= to)) {
+    x = - b - sqDsc / 2.0 * a;
+  }
+  /*if (!(x >= from && x <= to)) {
+    return false;
+  }*/
+  hit->t = x;
+  hit->point = r.origin + x * r.direction;
+  hit->normal = (hit->point - #{v center}) / #{radius}f;
+
+  return true;
+}
+
+|]
+
+rayToDoubles :: Ray -> [Double]
+rayToDoubles (Ray (CVec3 a b c) (CVec3 d e f)) = [a, b, c, d, e, f, -1, -1]
+
+doublesToHit :: [Double] -> Hit
+doublesToHit [t,a,b,c,x,y,z,-1] = Hit t (CVec3 a b c) (CVec3 x y z) undefined
+
+takeBy cnt [] = []
+takeBy cnt xs = (take cnt xs : takeBy cnt (drop cnt xs))
+
+-- runGeometryShader :: Shader -> [Ray] -> [Hit]
+runGeometryShader :: ShaderEngine -> Shader -> [Ray] -> IO [Hit]
+runGeometryShader e s rs = do
+  let rcnt = length rs
+  output <- runOnShader e s (rcnt * 8) (concatMap rayToDoubles rs)
+  return $ map doublesToHit $ takeBy 8 output
+
+main :: IO ()
+main = do
+  -- print $ compileSphere $ Sphere undefined (CVec3 0 0 (-1)) 0.5
+  engine <- initShaderEngine
+  let s = sphereSource ++ compileSphere "1" (Sphere undefined (CVec3 0 0 0) 2)
+  putStrLn s
+  shader <- createShader engine s
+  output <- runGeometryShader engine shader [Ray (CVec3 0 0 0) (CVec3 0 1 0), Ray (CVec3 7 8 9) (CVec3 10 11 12)]
+  putStrLn $ "Output: " ++ show output
---
+++ Parallel/Shaders.hs
@@ -0,0 +1,173 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Parallel.Shaders where
+
+import           Control.Parallel.CLUtil         (OpenCLState (OpenCLState),
+                                                  bufferToVector, clContext,
+                                                  clDevice, clQueue,
+                                                  writeVectorToBuffer)
+import           Control.Parallel.OpenCL
+
+import           Control.Monad                   (forM_)
+import           Data.Vector.Storable            (Vector)
+import qualified Data.Vector.Storable            as V
+import           Foreign                         (nullPtr, sizeOf)
+import           Foreign.C.Types                 (CFloat)
+import           Language.C.Quote.OpenCL         (cfun)
+import           Text.PrettyPrint.Mainland       (prettyCompact)
+import           Text.PrettyPrint.Mainland.Class (ppr)
+
+
+data ShaderEngine = ShaderEngine { openCLState :: OpenCLState }
+data Shader = Shader CLKernel
+
+initShaderEngine :: IO ShaderEngine
+initShaderEngine = do
+  context <- clCreateContextFromType [] [CL_DEVICE_TYPE_DEFAULT] print
+  device  <- last <$> clGetContextDevices context
+  -- pname device
+  dname device
+  queue   <- clCreateCommandQueue context device []
+
+  return $ ShaderEngine $ OpenCLState
+            { clDevice  = device
+            , clContext = context
+            , clQueue   = queue
+            }
+
+createShader :: ShaderEngine -> String -> IO Shader
+createShader engine source = do
+  let OpenCLState
+            { clDevice  = device
+            , clContext = context
+            , clQueue   = queue
+            } = openCLState engine
+  program <- clCreateProgramWithSource context source
+  clBuildProgram program [device] ""
+  kernel <- clCreateKernel program "doubleArray"
+  return $ Shader kernel
+
+  --
+
+runOnShader :: ShaderEngine -> Shader -> Int -> [Double] -> IO [Double]
+runOnShader engine shader outSize input = do
+  let OpenCLState
+            { clDevice  = device
+            , clContext = context
+            , clQueue   = queue
+            } = openCLState engine
+  let (Shader kernel) = shader
+  -- Set up memory
+  let
+      inputData :: Vector CFloat
+      inputData = V.fromList $ map realToFrac input
+
+      nElem  = V.length inputData
+      nInBytes = nElem * sizeOf (undefined :: CFloat)
+      nOutBytes = outSize * sizeOf (undefined :: CFloat)
+
+  -- Buffers for input and output data.
+  -- We request OpenCL to create a buffer on the host (CL_MEM_ALLOC_HOST_PTR)
+  -- since we're using CPU. The performance here may not be ideal, because
+  -- we're copying the buffer. However, it's safe, and not unduly nested.
+  bufIn <- clCreateBuffer context
+                          [CL_MEM_READ_ONLY, CL_MEM_ALLOC_HOST_PTR]
+                          (nInBytes, nullPtr)
+  bufOut <- clCreateBuffer context
+                           [CL_MEM_WRITE_ONLY, CL_MEM_ALLOC_HOST_PTR]
+                           (nOutBytes, nullPtr)
+
+  -- Copy our input data Vector to the input buffer; blocks until complete
+  writeVectorToBuffer (openCLState engine) bufIn inputData
+
+  -- Run the kernel
+  clSetKernelArgSto kernel 0 bufIn
+  clSetKernelArgSto kernel 1 bufOut
+  execEvent <- clEnqueueNDRangeKernel queue kernel [nElem] [] []
+
+  -- Get the result; blocks until complete
+  outputData <- bufferToVector queue
+                               bufOut
+                               outSize
+                               [execEvent]
+                               :: IO (Vector CFloat)
+
+  return $ map realToFrac $ V.toList outputData
+
+-- | The kernel to execute: the equivalient of 'map (*2)'.
+kernelSource :: String
+kernelSource = prettyCompact $ ppr $ [cfun|
+    /* This example kernel just does `map (*2)` */
+    kernel void doubleArray(
+        global float *in,
+        global float *out
+    ) {
+        int i = get_global_id(0);
+        out[i] = 2 * in[i];
+    }
+|]
+
+
+
+main :: IO ()
+main = do
+    putStrLn "* Hello World OpenCL Example *"
+
+    -- Describe the OpenCL Environment
+    putStrLn "\n* OpenCL Platform Environment *"
+    describePlatforms
+
+    -- Create a Context, Queue and a CLUtil OpenCLState
+    engine <- initShaderEngine
+    let state = openCLState engine
+    let OpenCLState
+              { clDevice  = device
+              , clContext = context
+              , clQueue   = queue
+              } = state
+
+    -- Create the Kernel
+    shader <- createShader engine kernelSource
+    let (Shader kernel) = shader
+
+
+    let inputData = [(-4) .. 4]
+    let outSize = length inputData
+
+    output <- runOnShader engine shader outSize inputData
+
+
+
+    -- Clean up the Context
+    _ <- clReleaseContext context
+
+    -- Show our work
+    putStrLn "\n* Results *"
+    putStrLn $ "Input:  " ++ show inputData
+    putStrLn $ "Output: " ++ show output
+
+
+
+-- | Summarises the OpenCL Platforms and their Devices.
+--
+--   The names of Platforms and all Devices belonging to them are printed to
+--   stdout.
+describePlatforms :: IO ()
+describePlatforms = do
+
+    -- fetch the list of OpenCL Platforms
+    platformList <- clGetPlatformIDs :: IO [CLPlatformID]
+
+    -- for each platform,
+    forM_ platformList $ \platform -> do
+
+        -- fetch the list of OpenCL Devices
+        devs <- clGetDeviceIDs platform CL_DEVICE_TYPE_ALL :: IO [CLDeviceID]
+
+        -- print the Platform name and Device names
+        pname platform
+        forM_ devs dname
+
+putPair name value = putStrLn (name ++ value)
+pname p = clGetPlatformInfo p CL_PLATFORM_NAME >>= putPair "Platform: "
+dname d = clGetDeviceName d                    >>= putPair "  Device: "
