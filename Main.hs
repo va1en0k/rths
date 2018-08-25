759024c1572e9b023a824260cec4b112d575674d
Valentin Golev <vgolev@trifacta.com>
2018-08-25 22:37:59 +0200

    normie

--- Main.hs
+++ Main.hs
@@ -25,8 +25,8 @@ import RayTracer
 
 import Render (res, kRes, camera, getRay, ImgBuf, colorToPixel)
 
--- import Render
-import Parallel.Render
+import Render
+-- import Parallel.Render
 
 import RTMonad
 
@@ -37,9 +37,26 @@ import WebGL
 import Material
 
 
-
 main :: IO ()
 main = do
+  world <- runRT undefined (randomWorld >> getWorld)
+  -- let world = randomWorld
+  -- let world = objects
+  imF <- genImageF world (fst res) (snd res)
+  let im = (generateImage imF (fst res) (snd res))
+  -- im <- evalRandIO rim
+  -- print (renderUV 0 0)
+  -- print (imF 10 10)
+  -- render 10 [] 10 10 >>= print
+  -- print (colorToPixel $ CVec3 0.8 0.7 1.0)
+  now <- getPOSIXTime
+  hash <- head <$> getArgs
+  writePng ("./out/image__" ++ (show now) ++ "__" ++ hash ++ ".png") im
+  writePng ("./image.png") im
+
+{-
+main' :: IO ()
+main' = do
   -- let world = randomWorld >> getWorld)
   -- let world = randomWorld
   -- let world = objects
@@ -54,10 +71,10 @@ main = do
   hash <- head <$> getArgs
   writePng ("./out/image__" ++ (show now) ++ "__" ++ hash ++ ".png") im
   writePng ("./image.png") im
+-}
 
-
-main' :: IO ()
-main' = do
+main'' :: IO ()
+main'' = do
   putStrLn $ types
   putStrLn $ cameraToWebgl camera
   putStrLn $ sphereToWebgl 1 (Sphere (mkLambertian $ CVec3 0.1 0.2 0.5) (CVec3 0 0 (-1)) 0.5)
