856a0223cc1f20e40aa897ec5566785b6171a5f1
Valentin Golev <vgolev@trifacta.com>
2018-08-28 16:33:48 +0200

    rev persp

--- Geometry/Camera.hs
+++ Geometry/Camera.hs
@@ -5,7 +5,7 @@ import           Data.Vec3
 import           Types
 import           Config
 
-camera = mkCamera (CVec3 18 5 10)
+camera = mkCamera (CVec3 18 5 14)
                   (CVec3 0 0 0)
                   (CVec3 0 (-1) 0)
                   20
