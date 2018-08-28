311e73ac78417bf3fe214e5f1814d3b5b61c584d
Valentin Golev <vgolev@trifacta.com>
2018-08-28 17:25:53 +0200

    three balls

--- World.hs
+++ World.hs
@@ -60,8 +60,8 @@ randomWorld = setWorld $ typical ++ hints -- ((typical ++) <$> concat <$> sequen
 
     typical = table ++
       [ sphere red (CVec3 (0.15) (-1.6) 8) 0.6
-      , sphere red (CVec3 0.3 (-0.4) 3.5) 0.6
-      , sphere red (CVec3 (-0.2) (0.3) 3.5) 0.6
+      , sphere red (CVec3 0.4 (-0.4) 3.5) 0.6
+      , sphere red (CVec3 (-0.7) (0.3) 3.5) 0.6
       ]
     typical' =
       [ --plane (mkLambertian $ CVec3 0.5 0.5 0.5) (V3 0 (-1) (-1)) (V3 0.02 1 (-0.3))
