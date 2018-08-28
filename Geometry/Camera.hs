d4134e8a26c65783719e2e6e85def19b764bd4bf
Valentin Golev <vgolev@trifacta.com>
2018-08-28 16:15:54 +0200

    ddebug rev persp

--- Geometry/Camera.hs
+++ Geometry/Camera.hs
@@ -5,20 +5,20 @@ import           Data.Vec3
 import           Types
 import           Config
 
--- camera = mkCamera (CVec3 13 4 6)
---                   (CVec3 0 0 0)
---                   (CVec3 0 (-1) 0)
---                   20
---                   (fromIntegral (fst res) / fromIntegral (snd res))
---                   10
+camera = mkCamera (CVec3 13 4 6)
+                  (CVec3 0 0 0)
+                  (CVec3 0 (-1) 0)
+                  20
+                  (fromIntegral (fst res) / fromIntegral (snd res))
+                  10
 
 -- camera = Camera {cOrigin = CVec3 13.0 2.0 (-11.0), cHorizontal = CVec3 4.555881805991546 0.0 5.384223952535464, cVertical = CVec3 0.3140143177281695 (-3.502467390044967) (-0.26570442269306643), cLowerLeftCorner = CVec3 2.983297972382686 2.584810007982875 (-7.143929486203352)}
 
-camera = Camera {
-  cOrigin = CVec3 (-10) 0 0,
-  cHorizontal = CVec3 32 0 0,
-  cVertical = CVec3 0 4 0,
-  cLowerLeftCorner = CVec3 (-4) (-2) (-3)}
+-- camera = Camera {
+--   cOrigin = CVec3 (-10) 0 0,
+--   cHorizontal = CVec3 32 0 0,
+--   cVertical = CVec3 0 4 0,
+--   cLowerLeftCorner = CVec3 (-4) (-2) (-3)}
 
 
 mkCamera :: CVec3 -> CVec3 -> CVec3 -> Double -> Double -> Double -> Camera
@@ -59,8 +59,8 @@ getCameraReflectedThroughUV c =
           <+> ((cVertical c) .^ 0.5)
           -- <-> (cOrigin c)
           )
-    (CVec3 x y z) = uvC .^ 2 <-> (cOrigin c)
-    (CVec3 _ y' _) = cOrigin c
+    (CVec3 x _ z) = uvC .^ 2 <-> (cOrigin c)
+    (CVec3 _ y _) = cOrigin c
   in CVec3 x y z
 
 
