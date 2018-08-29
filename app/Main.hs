595243a486ee7fb20a212bef35bc7593de986a74
Valentin Golev <vgolev@trifacta.com>
2018-08-29 19:54:29 +0200

    text

--- app/Main.hs
+++ app/Main.hs
@@ -90,7 +90,7 @@ textRender =
   do
     transpSurface (fromIntegral $ fst res) 120
 
-    setSourceRGB 1 0.5 0.2
+    setSourceRGB 0.1 0.5 0.2
     setLineWidth 3.0
     -- setLineCap LineCapRound
     -- setLineJoin LineJoinRound
