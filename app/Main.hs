eb452a598524b48cf333612b7a0ef3ceab93cddc
Valentin Golev <vgolev@trifacta.com>
2018-08-29 20:03:24 +0200

    text

--- app/Main.hs
+++ app/Main.hs
@@ -139,7 +139,7 @@ word32ToColor color = PixelRGB8 r g b where
   a = fromIntegral (shift (color .&. 0xFF000000) (-24))
   r = fromIntegral (shift (color .&. 0x00FF0000) (-16))
   g = fromIntegral (shift (color .&. 0x0000FF00) (-8))
-  b = fromIntegral (shift (color .&. 0x000000FF) (-24))
+  b = fromIntegral (shift (color .&. 0x000000FF) (-0))
 
 
 main :: IO ()
